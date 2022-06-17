0129a3f41a04e804ad8c2503c47fdbf2f75c6ef2
Sergei Tomilov <tsv.titan@gmail.com>
2022-06-17 19:08:09 +0300

    Minor config changes

--- test/test.sh
+++ test/test.sh
@@ -7,7 +7,7 @@
 
 #curl -sk -X POST -H "Content-type: application/json" -d @alertmanager.json "http://localhost:8081/alertmanager"
 
-#curl -sk -X POST -H "Content-type: application/json" -d @datadog.json "http://localhost:8081/datadog"
+curl -sk -X POST -H "Content-type: application/json" -d @datadog.json "http://localhost:8081/datadog"
 
 #curl -sk -X POST -H "Content-type: application/json" -d @site24x7.json "http://localhost:8081/site24x7"
 
@@ -15,4 +15,4 @@
 
 #curl -sk -X POST -H "Content-type: application/json" -d @google.json "http://localhost:8081/google"
 
-curl -sk -X POST -H "Content-type: application/json" -d @aws.json "http://localhost:8081/aws.amazon.com"
+#curl -sk -X POST -H "Content-type: application/json" -d @aws.json "http://localhost:8081/aws.amazon.com"
