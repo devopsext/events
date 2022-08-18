e7ae2dea82aa8e48fba02a98b0613deb5a89450e
askripe <99725186+askripe@users.noreply.github.com>
2022-08-18 09:57:52 +0300

    add zabbix support (#20)
    * add zabbix support for ZabbixEvent

--- test/test.sh
+++ test/test.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-#curl -sk -X POST -H "Content-type: application/json" -H "X-Gitlab-Event: Job Hook" -d @gitlab-job.json "http://localhost:8081/gitlab"
-#curl -sk -X POST -H "Content-type: application/json" -H "X-Gitlab-Event: Pipeline Hook" -d @gitlab-pipeline.json "http://localhost:8081/gitlab"
+#curl -sk -X POST -H "Content-type: application/json" -H "X-Gitlab-Event: Job Hook" -d @gitlab-job.json "http://localhost:80/gitlab"
+#curl -sk -X POST -H "Content-type: application/json" -H "X-Gitlab-Event: Pipeline Hook" -d @gitlab-pipeline.json "http://localhost:80/gitlab"
 
-#curl -sk -X POST -H "Content-type: application/json" -d @k8s.json "http://localhost:8081/k8s"
+#curl -sk -X POST -H "Content-type: application/json" -d @k8s.json "http://localhost:80/k8s"
 
-#curl -sk -X POST -H "Content-type: application/json" -d @alertmanager.json "http://localhost:8081/alertmanager"
+#curl -sk -X POST -H "Content-type: application/json" -d @alertmanager.json "http://localhost:80/alertmanager"
+curl -sk -X POST -H "Content-type: application/json" -d @zabbix.json "http://localhost:80/zabbix"
 
-curl -sk -X POST -H "Content-type: application/json" -d @datadog-triggered.json "http://localhost:8081/datadog"
-#curl -sk -X POST -H "Content-type: application/json" -d @datadog-recovered.json "http://localhost:8081/datadog"
+#curl -sk -X POST -H "Content-type: application/json" -d @datadog-triggered.json "http://localhost:80/datadog"
+#curl -sk -X POST -H "Content-type: application/json" -d @datadog-recovered.json "http://localhost:80/datadog"
 
-#curl -sk -X POST -H "Content-type: application/json" -d @site24x7.json "http://localhost:8081/site24x7"
+#curl -sk -X POST -H "Content-type: application/json" -d @site24x7.json "http://localhost:80/site24x7"
 
-#curl -sk -X POST -H "Content-type: application/json" -d @cloudflare.json "http://localhost:8081/cloudflare"
+#curl -sk -X POST -H "Content-type: application/json" -d @cloudflare.json "http://localhost:80/cloudflare"
 
-#curl -sk -X POST -H "Content-type: application/json" -d @google.json "http://localhost:8081/google"
+#curl -sk -X POST -H "Content-type: application/json" -d @google.json "http://localhost:80/google"
 
-#curl -sk -X POST -H "Content-type: application/json" -d @aws.json "http://localhost:8081/aws.amazon.com"
+#curl -sk -X POST -H "Content-type: application/json" -d @aws.json "http://localhost:80/aws.amazon.com"
